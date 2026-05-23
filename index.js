e6bb750051af3d9ea7675a99bd1433de9f8d5973
Kevin Guerra <guerraroncancio@gmail.com>
2026-05-23 17:34:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ console.clear()
 cfonts.say('GUERRA BOT', {
     font: 'block',
     align: 'center',
-    gradient: ['#00F5FF', '#7F00FF']
+   gradient: ['#00F5FF', '#7F00FF']
 })
 
 console.log(
