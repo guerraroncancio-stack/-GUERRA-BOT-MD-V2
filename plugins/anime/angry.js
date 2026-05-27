a250db1c2013f60979197f1dc62a2527e21370e0
Kevin Guerra <guerraroncancio@gmail.com>
2026-05-27 02:00:41 -0500

    Update angry.js

--- plugins/anime/angry.js
+++ plugins/anime/angry.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const reaction = {
     emoji: '😡',
-    txt_solo: '> ❒ @user1 está enojado con todo el grupo... 😡',
-    txt_mencion: '> ❏ @user1 está enojado con @user2 😡',
+    txt_solo: '╭─❒ @user1\n│ 😡 Está enojado con todo el grupo...\n╰───────────────',
+    txt_mencion: '╭─❏ @user1\n│ 😡 Está enojado con @user2\n╰───────────────',
     links: [
         'https://media.tenor.com/tx3x8ANgbBwAAAPo/the-dreaming-boy-is-a-realist-yumemiru-danshi.mp4',
         'https://media.tenor.com/pbqNBWOx6xUAAAPo/annoyed-anime-girl-annoyed.mp4',
@@ -14,6 +14,7 @@ const reaction = {
         'httpss//media.tenor.com/Rxjl-XIiekMAAAPo/angry.mp4',
         'https://media.tenor.com/qiOZauqDU8gAAAPo/mad-angry.mp4',
         'https://media.tenor.com/9JjBiqaxzdAAAAPo/anime-angry.mp4',
+        'https://media.tenor.com/tx3x8ANgbBwAAAPo/the-dreaming-boy-is-a-realist-yumemiru-danshi.mp4',
         'https://media.tenor.com/U8vM8y9oJjUAAAPo/nisekoi-chitoge-kirisaki.mp4',
         'https://media.tenor.com/5hCo-bxm3mUAAAPo/gojo-gojo-annoyed.mp4',
         'https://media.tenor.com/z2iFD-hLYnAAAAPo/anime-girl-anime.mp4'
@@ -24,53 +25,48 @@ const angry = {
     name: 'angry',
     alias: ['enojado', 'angry'],
     category: 'interacciones',
-
     run: async (m, { conn }) => {
+        if (!reaction.links.length) return
+
+        const user1 = m.sender
+        const user2 = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : null)
+
+        const name1 = '@' + user1.split('@')[0]
+        const menciones = [user1]
+
+        let textoFinal = ''
+
+        if (user2) {
+            menciones.push(user2)
+            const name2 = '@' + user2.split('@')[0]
+
+            textoFinal = reaction.txt_mencion
+                .replace(/@user1/g, name1)
+                .replace(/@user2/g, name2)
+
+        } else {
+            textoFinal = reaction.txt_solo.replace(/@user1/g, name1)
+        }
+
         try {
-            if (!reaction.links.length) return
-
-            const sender = m.sender?.split('@')[0]
-            const target = m.mentionedJid?.[0]?.split('@')[0]
-                || m.quoted?.sender?.split('@')[0]
-
-            const user1 = `@${sender}`
-            const user2 = target ? `@${target}` : null
-
-            const mentions = [m.sender]
-            let caption
-
-            if (user2) {
-                mentions.push(m.mentionedJid?.[0] || m.quoted?.sender)
-                caption = reaction.txt_mencion
-                    .replace(/@user1/g, user1)
-                    .replace(/@user2/g, user2)
-            } else {
-                caption = reaction.txt_solo.replace(/@user1/g, user1)
-            }
-
             if (m.react) await m.react(reaction.emoji)
 
-            const video = reaction.links[
+            const videoUrl = reaction.links[
                 Math.floor(Math.random() * reaction.links.length)
             ]
 
             await conn.sendMessage(m.chat, {
-                video: { url: video },
-                caption: `🔥 ${caption}`,
+                video: { url: videoUrl },
+                caption: `🔥 𝗘𝗡𝗢𝗝𝗢 𝗠𝗢𝗗 🔥\n\n${textoFinal}`,
                 gifPlayback: true,
-                mentions,
+                mentions: menciones,
                 contextInfo: {
-                    externalAdReply: {
-                        title: '😡 Reacción Angry',
-                        body: 'Sistema de interacciones',
-                        previewType: 'PHOTO'
-                    },
                     ...channelInfo
                 }
             }, { quoted: m })
 
-        } catch (err) {
-            console.error('[ANGRY ERROR]', err)
+        } catch (e) {
+            console.error(e)
         }
     }
 }
