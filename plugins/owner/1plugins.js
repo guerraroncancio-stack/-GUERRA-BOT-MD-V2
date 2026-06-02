b5a9a8ab8e3d2a3f022dbac3b7084193f729ef91
Kevin Guerra <guerraroncancio@gmail.com>
2026-06-02 02:09:36 -0500

    Update 1plugins.js

--- plugins/owner/1plugins.js
+++ plugins/owner/1plugins.js
@@ -6,22 +6,9 @@ const pluginsCmd = {
     run: async (m) => {
 
         const plugins = global.plugins || {}
-        const list = Object.keys(plugins)
+        const total = Object.keys(plugins).length
 
-        if (!list.length) {
-            return m.reply('⚠️ No hay plugins cargados.')
-        }
-
-        let txt = `📦 *PLUGINS SYSTEM*\n\n`
-        txt += `🔢 Total de plugins: ${list.length}\n\n`
-
-        txt += `📂 Lista:\n`
-
-        for (const p of list) {
-            txt += `• ${p}\n`
-        }
-
-        return m.reply(txt)
+        return m.reply(`📦 *Plugins cargados:* ${total}`)
     }
 }
 
