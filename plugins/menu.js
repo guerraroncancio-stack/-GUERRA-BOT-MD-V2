f1985c706aa87a3e9bf9e75879cd131d59e44828
Kevin Guerra <guerraroncancio@gmail.com>
2026-06-10 02:17:41 -0500

    Add thumbnail fetching for video message
    Fetch and set thumbnail for video message.

--- plugins/menu.js
+++ plugins/menu.js
@@ -155,13 +155,29 @@ const menu = `
   const fkontak =
   await makeFkontak()
 
-  await conn.sendMessage(
+const res = await fetch(
+'https://cdn.dix.lat/me/tmp/7b22e418-15b3-4b92-b9e1-4b767b8e6577.jpg'
+)
+
+const thumb = Buffer.from(
+await res.arrayBuffer()
+)
+
+await conn.sendMessage(
   m.chat,
   {
     video: {
       url: 'https://cdn.dix.lat/me/99ec88ba-77e9-4bda-b8a6-c777e271249a.mp4'
     },
+
     gifPlayback: true,
+
+    jpegThumbnail: thumb,
+
+    thumbnail: thumb,
+
+    mimetype: 'video/mp4',
+
     caption: menu
   },
   {
